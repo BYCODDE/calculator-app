42399aa8c12fc8032bd4d6a068e9a7263ab13988
BYCODDE <anaraliev217@gmail.com>
2024-04-01 22:18:20 +0400

    working on theme!

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ delButton.addEventListener("click", () => {
 // main_buttons.forEach((button) => {
 //   console.log(button);
 // });
+
+const myRange = document.getElementById("myRange");
+const top_Section = document.querySelector(".top_section");
+const middle_section = document.querySelector(".middle_section");
+const bottom_section = document.querySelector(".bottom_section");
+
+const h3_calc = document.getElementById("h3_calc");
+const THEME_div = document.querySelector(".THEME_div h4");
+
+myRange.addEventListener("input", function () {
+  if (myRange.value === "1") {
+    // Change the background color of the body to red
+    document.body.style.backgroundColor = "rgb(230, 230, 230)";
+    top_Section.style.backgroundColor = "rgb(230, 230, 230)";
+    middle_section.style.backgroundColor = "rgb(238, 238, 238)";
+    bottom_section.style.backgroundColor = "rgba(210, 205, 205, 1)";
+    h3_calc.style.color = "rgba(54, 54, 44, 1)";
+    THEME_div.style.color = "rgba(54, 54, 44, 1)";
+  } else {
+    document.body.style.backgroundColor = "";
+    top_Section.style.backgroundColor = "";
+    middle_section.style.backgroundColor = "";
+    bottom_section.style.backgroundColor = "";
+    h3_calc.style.color = "";
+    THEME_div.style.color = "";
+  }
+});
