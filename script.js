85a185e00d67bad72b82b31d47e74447a110ad3d
BYCODDE <anaraliev217@gmail.com>
2024-04-01 22:33:14 +0400

    working on theme!

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const resetButton = document.getElementById("blue_button2");
 const delButton = document.getElementById("blue_button");
 let number = 0;
 h2Display.textContent = number;
+const buttons = main_buttons.forEach((button) => {});
 
 resetButton.addEventListener("click", () => {
   h2Display.textContent = number;
@@ -15,10 +16,6 @@ delButton.addEventListener("click", () => {
   h2Display.textContent = h2Display.textContent.slice(0, -1);
 });
 
-// main_buttons.forEach((button) => {
-//   console.log(button);
-// });
-
 const myRange = document.getElementById("myRange");
 const top_Section = document.querySelector(".top_section");
 const middle_section = document.querySelector(".middle_section");
@@ -26,16 +23,20 @@ const bottom_section = document.querySelector(".bottom_section");
 
 const h3_calc = document.getElementById("h3_calc");
 const THEME_div = document.querySelector(".THEME_div h4");
-
+const THEME_filter = document.querySelector(".THEME_filter output ");
+const THEME_filter2 = document.querySelector(".THEME_filter input");
 myRange.addEventListener("input", function () {
   if (myRange.value === "1") {
-    // Change the background color of the body to red
     document.body.style.backgroundColor = "rgb(230, 230, 230)";
     top_Section.style.backgroundColor = "rgb(230, 230, 230)";
     middle_section.style.backgroundColor = "rgb(238, 238, 238)";
     bottom_section.style.backgroundColor = "rgba(210, 205, 205, 1)";
     h3_calc.style.color = "rgba(54, 54, 44, 1)";
     THEME_div.style.color = "rgba(54, 54, 44, 1)";
+    THEME_filter.style.color = "rgba(54, 54, 44, 1)";
+    THEME_filter2.style.backgroundColor = "rgba(210, 205, 205, 1)";
+    h2Display.style.color = "rgba(54, 54, 44, 1)";
+    buttons.style.color = "rgba(54, 54, 44, 1)";
   } else {
     document.body.style.backgroundColor = "";
     top_Section.style.backgroundColor = "";
@@ -43,5 +44,8 @@ myRange.addEventListener("input", function () {
     bottom_section.style.backgroundColor = "";
     h3_calc.style.color = "";
     THEME_div.style.color = "";
+    THEME_filter.style.color = "";
+    THEME_filter2.style.backgroundColor = "";
+    h2Display.style.color = "";
   }
 });
