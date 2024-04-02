50b83c4883661e13a0b029138d35ed40e33083ba
BYCODDE <anaraliev217@gmail.com>
2024-04-02 19:57:37 +0400

    js logic!

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const h2Display = document.getElementById("main_h2");
 const main_buttons = document.querySelectorAll(".main_buttons button ");
 const resetButton = document.getElementById("blue_button2");
 const delButton = document.getElementById("blue_button");
-const addNumber = document.getElementById("+");
-const sum = document.getElementById("red_button");
-
+const sumNumber = document.getElementById("+");
+const Equality = document.getElementById("red_button");
+const Subtract = document.getElementById("-");
+const Divide = document.getElementById("/");
+const Multiply = document.getElementById("x");
+console.log(Subtract, Divide, Multiply);
 myRange.addEventListener("input", function () {
   if (myRange.value === "1") {
     body.classList.add("level1");
@@ -39,15 +42,28 @@ main_buttons.forEach((button) => {
     }
   });
 });
-sum.addEventListener("click", function () {
-  if (h2Display.textContent.length >= 2 && addNumber.textContent === "+") {
+Equality.addEventListener("click", function () {
+  if (h2Display.textContent.length >= 2 && sumNumber.textContent === "+") {
     const mainSplit = Number(h2Display.textContent) + Number(firstInput);
     h2Display.textContent = mainSplit;
   }
+  else if(h2Display.textContent.length >= 2 && Subtract.textContent === "-"){
+    const mainSplit2 = Number(h2Display.textContent) - Number(firstInput);
+    console.log(mainSplit2);
+    h2Display.textContent = mainSplit2;
+
+  }
 });
 
-addNumber.addEventListener("click", function () {
-  if (h2Display.textContent.length >= 2 && addNumber.textContent === "+") {
+sumNumber.addEventListener("click", function () {
+  if (h2Display.textContent.length >= 2 && sumNumber.textContent === "+") {
+    firstInput = h2Display.textContent;
+    h2Display.textContent = "";
+  }
+});
+
+Subtract.addEventListener("click", function () {
+  if (h2Display.textContent.length >= 2 && Subtract.textContent === "-") {
     firstInput = h2Display.textContent;
     h2Display.textContent = "";
   }
