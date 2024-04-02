bd0b771c21a6aea2963afedc97fd1de2e194cc47
BYCODDE <anaraliev217@gmail.com>
2024-04-02 15:10:50 +0400

    working on js logic!

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 const body = document.querySelector("body");
 
-const h2Display = document.getElementById("main_h2");
-const main_buttons = document.querySelectorAll(".main_buttons button ");
-const resetButton = document.getElementById("blue_button2");
-const delButton = document.getElementById("blue_button");
-let number = 0;
-h2Display.textContent = number;
-const buttons = main_buttons.forEach((button) => {});
-
-resetButton.addEventListener("click", () => {
-  h2Display.textContent = number;
-  console.log("clicked");
-});
-
-delButton.addEventListener("click", () => {
-  console.log("clicked");
-  h2Display.textContent = h2Display.textContent.slice(0, -1);
-});
-
 const myRange = document.getElementById("myRange");
 const top_Section = document.querySelector(".top_section");
 const middle_section = document.querySelector(".middle_section");
@@ -30,31 +12,55 @@ const THEME_filter2 = document.querySelector(".THEME_filter input");
 myRange.addEventListener("input", function () {
   if (myRange.value === "1") {
     body.classList.add("level1");
-    // document.body.style.backgroundColor = "rgb(230, 230, 230)";
-    // top_Section.style.backgroundColor = "rgb(230, 230, 230)";
-    // middle_section.style.backgroundColor = "rgb(238, 238, 238)";
-    // bottom_section.style.backgroundColor = "rgba(210, 205, 205, 1)";
-    // h3_calc.style.color = "rgba(54, 54, 44, 1)";
-    // THEME_div.style.color = "rgba(54, 54, 44, 1)";
-    // THEME_filter.style.color = "rgba(54, 54, 44, 1)";
-    // THEME_filter2.style.backgroundColor = "rgba(210, 205, 205, 1)";
-    // h2Display.style.color = "rgba(54, 54, 44, 1)";
-    // buttons.style.color = "rgba(54, 54, 44, 1)";
   } else if (myRange.value === "2") {
     body.classList.remove("level1");
     body.classList.add("level2");
-    // document.body.style.backgroundColor = "";
-    // top_Section.style.backgroundColor = "";
-    // middle_section.style.backgroundColor = "";
-    // bottom_section.style.backgroundColor = "";
-    // h3_calc.style.color = "";
-    // THEME_div.style.color = "";
-    // THEME_filter.style.color = "";
-    // THEME_filter2.style.backgroundColor = "";
-    // h2Display.style.color = "";
+  } else {
+    body.classList.remove("level1");
+    body.classList.remove("level2");
   }
-  else{
-    body.classList.remove("level1")
-    body.classList.remove("level2")
-  }
+});
+
+/* FIXME:RESET button hover needs to be fixed and classname logic should be checked again! */
+
+//BUTTON LOGIC!
+const h2Display = document.getElementById("main_h2");
+const main_buttons = document.querySelectorAll(".main_buttons button ");
+const resetButton = document.getElementById("blue_button2");
+const delButton = document.getElementById("blue_button");
+// let number = 0;
+// let convert = Number((h2Display.textContent = number));
+resetButton.addEventListener("click", () => {
+  h2Display.textContent = 0;
+});
+
+delButton.addEventListener("click", () => {
+  h2Display.textContent = h2Display.textContent.slice(0, -1);
+});
+
+const buttons = main_buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    if (button.textContent === "7") {
+      h2Display.textContent = +7;
+    } else if (button.textContent === "8") {
+      h2Display.textContent = +8;
+    } else if (button.textContent === "9") {
+      h2Display.textContent = +9;
+    }else if (button.textContent === "4") {
+      h2Display.textContent = +4;
+    }else if (button.textContent === "5") {
+      h2Display.textContent = +5;
+    }else if (button.textContent === "6") {
+      h2Display.textContent = +6;
+    }else if (button.textContent === "1") {
+      h2Display.textContent = +1;
+    }else if (button.textContent === "2") {
+      h2Display.textContent = +2;
+    }else if (button.textContent === "3") {
+      h2Display.textContent = +3;
+    }else if (button.textContent === "0") {
+      h2Display.textContent = +0;
+    }
+  });
+  console.log(button);
 });
